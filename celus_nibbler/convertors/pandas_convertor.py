4bf3e9a3779e4d93ba643e6d9f45e4abe43396c4
Zbynek Vyhlas <zbyneksmail@gmail.com>
2021-08-26 14:47:01 +0200

    exporters to csv

---
+++ celus_nibbler/convertors/pandas_convertor.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+import pandas as pd
+
+start_time = datetime.now()
+
+my_file = 'Anne'
+my_format = 'xlsx'
+read_file = pd.read_excel(
+    f"/Users/Zbynek/Documents/MyDocuments/BDD/projekty/Nibbler/celus-nibbler/celus_nibbler/parsers/convertors/{my_file}.{my_format}"
+)
+
+read_file.to_csv(
+    f"/Users/Zbynek/Documents/MyDocuments/BDD/projekty/Nibbler/celus-nibbler/celus_nibbler/parsers/convertors/{my_format}_format_export_by_pandas.csv",
+    index=None,
+    header=True,
+)
+
+
+end_time = datetime.now()
+duration = end_time - start_time
+
+print(f"format: {my_format}")
+print(f"filename: {my_file}")
+print("pandas")
+print(f"duration: {duration}")
