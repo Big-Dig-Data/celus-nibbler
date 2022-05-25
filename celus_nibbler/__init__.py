8f0d547eb6dfa13ddffcf59d97571e5c6184189a
Stepan Henek <stepan@bigdigdata.com>
2022-05-25 10:05:50 +0200

    feature: extend main __init__.py imports

--- celus_nibbler/__init__.py
+++ celus_nibbler/__init__.py
@@ -1,4 +1,24 @@
-from .eat_and_poop import Poop, eat, findparser  # noqa
-from .errors import RecordError  # noqa
-from .parsers import get_supported_platforms, get_supported_platforms_count  # noqa
-from .record import CounterRecord  # noqa
+from .eat_and_poop import Poop, eat
+from .errors import (
+    MultipleParsersFound,
+    NibblerError,
+    NoParserFound,
+    RecordError,
+    TableException,
+    WrongFileFormatError,
+)
+from .parsers import get_supported_platforms
+from .record import CounterRecord
+
+__all__ = [
+    'eat',
+    'get_supported_platforms',
+    'Poop',
+    'CounterRecord',
+    'MultipleParsersFound',
+    'NibblerError',
+    'NoParserFound',
+    'RecordError',
+    'TableException',
+    'WrongFileFormatError',
+]
