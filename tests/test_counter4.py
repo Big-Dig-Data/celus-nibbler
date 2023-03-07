8b65aa770826c2dab4968bb7636d1ea6be00bad7
Stepan Henek <stepan@bigdigdata.com>
2023-03-07 23:04:31 +0100

    chore: add tests where `use_heuristics=True` fails the parsing and `use_heuristics=False` passes

--- tests/test_counter4.py
+++ tests/test_counter4.py
@@ -4,44 +4,60 @@ import pathlib
 import pytest
 
 from celus_nibbler import eat
+from celus_nibbler.errors import NoParserMatchesHeuristics
 
 
 @pytest.mark.parametrize(
-    "platform,file,parser,heuristics",
+    "platform,file,parser,heuristics,success",
     (
-        ("Ovid", "4/BR1-a.tsv", "static.counter4.BR1.Tabular", True),
-        ("Ovid", "4/BR1-b.tsv", "static.counter4.BR1.Tabular", False),
-        ("Ovid", "4/BR1-empty1.tsv", "static.counter4.BR1.Tabular", True),
-        ("Ovid", "4/BR1-empty2.tsv", "static.counter4.BR1.Tabular", True),
-        ("Ovid", "4/BR1-empty_values.csv", "static.counter4.BR1.Tabular", True),
-        ("WileyOnlineLibrary", "4/BR2-a.tsv", "static.counter4.BR2.Tabular", True),
-        ("WileyOnlineLibrary", "4/BR2-b.tsv", "static.counter4.BR2.Tabular", False),
-        ("Psychiatry Online", "4/BR3-a.tsv", "static.counter4.BR3.Tabular", True),
-        ("Psychiatry Online", "4/BR3-b.tsv", "static.counter4.BR3.Tabular", False),
-        ("Tandfonline", "4/DB1-a.tsv", "static.counter4.DB1.Tabular", True),
-        ("Tandfonline", "4/DB1-b.tsv", "static.counter4.DB1.Tabular", False),
-        ("Tandfonline", "4/DB2-a.tsv", "static.counter4.DB2.Tabular", True),
-        ("Tandfonline", "4/DB2-b.tsv", "static.counter4.DB2.Tabular", False),
-        ("ProQuest", "4/PR1-a.tsv", "static.counter4.PR1.Tabular", True),
-        ("ProQuest", "4/PR1-b.tsv", "static.counter4.PR1.Tabular", False),
-        ("Thieme", "4/JR1-a.tsv", "static.counter4.JR1.Tabular", True),
-        ("Thieme", "4/JR1-b.tsv", "static.counter4.JR1.Tabular", False),
-        ("Sage", "4/JR1a-a.tsv", "static.counter4.JR1a.Tabular", True),
-        ("Sage", "4/JR1a-b.tsv", "static.counter4.JR1a.Tabular", False),
-        ("OUP", "4/JR1GOA-a.tsv", "static.counter4.JR1GOA.Tabular", True),
-        ("OUP", "4/JR1GOA-b.tsv", "static.counter4.JR1GOA.Tabular", False),
-        ("ProQuest", "4/JR2-a.tsv", "static.counter4.JR2.Tabular", True),
-        ("ProQuest", "4/JR2-b.tsv", "static.counter4.JR2.Tabular", False),
-        ("ProQuest", "4/MR1-a.tsv", "static.counter4.MR1.Tabular", True),
-        ("ProQuest", "4/MR1-b.tsv", "static.counter4.MR1.Tabular", False),
+        ("Ovid", "4/BR1-a.tsv", "static.counter4.BR1.Tabular", True, True),
+        ("Ovid", "4/BR1-b.tsv", "static.counter4.BR1.Tabular", False, True),
+        ("Ovid", "4/BR1-b.tsv", "static.counter4.BR1.Tabular", True, False),
+        ("Ovid", "4/BR1-empty1.tsv", "static.counter4.BR1.Tabular", True, True),
+        ("Ovid", "4/BR1-empty2.tsv", "static.counter4.BR1.Tabular", True, True),
+        ("Ovid", "4/BR1-empty_values.csv", "static.counter4.BR1.Tabular", True, True),
+        ("WileyOnlineLibrary", "4/BR2-a.tsv", "static.counter4.BR2.Tabular", True, True),
+        ("WileyOnlineLibrary", "4/BR2-b.tsv", "static.counter4.BR2.Tabular", False, True),
+        ("WileyOnlineLibrary", "4/BR2-b.tsv", "static.counter4.BR2.Tabular", True, False),
+        ("Psychiatry Online", "4/BR3-a.tsv", "static.counter4.BR3.Tabular", True, True),
+        ("Psychiatry Online", "4/BR3-b.tsv", "static.counter4.BR3.Tabular", False, True),
+        ("Psychiatry Online", "4/BR3-b.tsv", "static.counter4.BR3.Tabular", True, False),
+        ("Tandfonline", "4/DB1-a.tsv", "static.counter4.DB1.Tabular", True, True),
+        ("Tandfonline", "4/DB1-b.tsv", "static.counter4.DB1.Tabular", False, True),
+        ("Tandfonline", "4/DB1-b.tsv", "static.counter4.DB1.Tabular", True, False),
+        ("Tandfonline", "4/DB2-a.tsv", "static.counter4.DB2.Tabular", True, True),
+        ("Tandfonline", "4/DB2-b.tsv", "static.counter4.DB2.Tabular", False, True),
+        ("Tandfonline", "4/DB2-b.tsv", "static.counter4.DB2.Tabular", True, False),
+        ("ProQuest", "4/PR1-a.tsv", "static.counter4.PR1.Tabular", True, True),
+        ("ProQuest", "4/PR1-b.tsv", "static.counter4.PR1.Tabular", False, True),
+        ("ProQuest", "4/PR1-b.tsv", "static.counter4.PR1.Tabular", True, False),
+        ("Thieme", "4/JR1-a.tsv", "static.counter4.JR1.Tabular", True, True),
+        ("Thieme", "4/JR1-b.tsv", "static.counter4.JR1.Tabular", False, True),
+        ("Thieme", "4/JR1-b.tsv", "static.counter4.JR1.Tabular", True, False),
+        ("Sage", "4/JR1a-a.tsv", "static.counter4.JR1a.Tabular", True, True),
+        ("Sage", "4/JR1a-b.tsv", "static.counter4.JR1a.Tabular", False, True),
+        ("Sage", "4/JR1a-b.tsv", "static.counter4.JR1a.Tabular", True, False),
+        ("OUP", "4/JR1GOA-a.tsv", "static.counter4.JR1GOA.Tabular", True, True),
+        ("OUP", "4/JR1GOA-b.tsv", "static.counter4.JR1GOA.Tabular", False, True),
+        ("OUP", "4/JR1GOA-b.tsv", "static.counter4.JR1GOA.Tabular", True, False),
+        ("ProQuest", "4/JR2-a.tsv", "static.counter4.JR2.Tabular", True, True),
+        ("ProQuest", "4/JR2-b.tsv", "static.counter4.JR2.Tabular", False, True),
+        ("ProQuest", "4/JR2-b.tsv", "static.counter4.JR2.Tabular", True, False),
+        ("ProQuest", "4/MR1-a.tsv", "static.counter4.MR1.Tabular", True, True),
+        ("ProQuest", "4/MR1-b.tsv", "static.counter4.MR1.Tabular", False, True),
+        ("ProQuest", "4/MR1-b.tsv", "static.counter4.MR1.Tabular", True, False),
     ),
 )
-def test_tsv(platform, file, parser, heuristics):
+def test_tsv(platform, file, parser, heuristics, success):
     source_path = pathlib.Path(__file__).parent / 'data/counter' / file
     output_path = pathlib.Path(__file__).parent / 'data/counter' / f"{file}.out"
     with output_path.open() as f:
         reader = csv.reader(f)
         poop = eat(source_path, platform, parsers=[parser], use_heuristics=heuristics)[0]
+        if not success:
+            assert isinstance(poop, NoParserMatchesHeuristics)
+            return
+
         for record in poop.records():
             assert next(reader) == list(record.as_csv()), "Compare lines"
 
--- tests/test_counter5.py
+++ tests/test_counter5.py
@@ -4,39 +4,52 @@ import pathlib
 import pytest
 
 from celus_nibbler import eat
+from celus_nibbler.errors import NoParserMatchesHeuristics
 
 
 @pytest.mark.parametrize(
-    "platform,file,parser,heuristics",
+    "platform,file,parser,heuristics,success",
     (
-        ("WebOfKnowledge", "5/DR-a.tsv", "static.counter5.DR.Tabular", True),
-        ("WebOfKnowledge", "5/DR-b.tsv", "static.counter5.DR.Tabular", False),
-        ("WebOfKnowledge", "5/DR-c.json", "static.counter5.DR.Json", True),
-        ("WebOfKnowledge", "5/DR-d.json", "static.counter5.DR.Json", False),
-        ("WebOfKnowledge", "5/DR-e.csv", "static.counter5.DR.Tabular", True),
-        ("MIT", "5/PR-a.tsv", "static.counter5.PR.Tabular", True),
-        ("MIT", "5/PR-b.tsv", "static.counter5.PR.Tabular", False),
-        ("MIT", "5/PR-c.json", "static.counter5.PR.Json", True),
-        ("MIT", "5/PR-d.json", "static.counter5.PR.Json", False),
-        ("Ovid", "5/PR-e.csv", "static.counter5.PR.Tabular", False),
-        ("MIT", "5/TR-a.tsv", "static.counter5.TR.Tabular", True),
-        ("MIT", "5/TR-b.tsv", "static.counter5.TR.Tabular", False),
-        ("MIT", "5/TR-c.json", "static.counter5.TR.Json", True),
-        ("MIT", "5/TR-d.json", "static.counter5.TR.Json", False),
-        ("MIT", "5/TR-e.tsv", "static.counter5.TR.Tabular", True),
-        ("T&F ebooks", "5/TR_B1-a.xlsx", "static.counter5.TR.Tabular", True),
-        ("JSTOR", "5/IR_M1-a.csv", "static.counter5.IR_M1.Tabular", True),
-        ("JSTOR", "5/IR_M1-b.csv", "static.counter5.IR_M1.Tabular", False),
-        ("JSTOR", "5/IR_M1-c.json", "static.counter5.IR_M1.Json", True),
-        ("JSTOR", "5/IR_M1-d.json", "static.counter5.IR_M1.Json", False),
+        ("WebOfKnowledge", "5/DR-a.tsv", "static.counter5.DR.Tabular", True, True),
+        ("WebOfKnowledge", "5/DR-b.tsv", "static.counter5.DR.Tabular", False, True),
+        ("WebOfKnowledge", "5/DR-b.tsv", "static.counter5.DR.Tabular", True, False),
+        ("WebOfKnowledge", "5/DR-c.json", "static.counter5.DR.Json", True, True),
+        ("WebOfKnowledge", "5/DR-d.json", "static.counter5.DR.Json", False, True),
+        ("WebOfKnowledge", "5/DR-d.json", "static.counter5.DR.Json", True, False),
+        ("WebOfKnowledge", "5/DR-e.csv", "static.counter5.DR.Tabular", True, True),
+        ("MIT", "5/PR-a.tsv", "static.counter5.PR.Tabular", True, True),
+        ("MIT", "5/PR-b.tsv", "static.counter5.PR.Tabular", False, True),
+        ("MIT", "5/PR-b.tsv", "static.counter5.PR.Tabular", True, False),
+        ("MIT", "5/PR-c.json", "static.counter5.PR.Json", True, True),
+        ("MIT", "5/PR-d.json", "static.counter5.PR.Json", False, True),
+        ("MIT", "5/PR-d.json", "static.counter5.PR.Json", True, False),
+        ("Ovid", "5/PR-e.csv", "static.counter5.PR.Tabular", True, True),
+        ("MIT", "5/TR-a.tsv", "static.counter5.TR.Tabular", True, True),
+        ("MIT", "5/TR-b.tsv", "static.counter5.TR.Tabular", False, True),
+        ("MIT", "5/TR-b.tsv", "static.counter5.TR.Tabular", True, False),
+        ("MIT", "5/TR-c.json", "static.counter5.TR.Json", True, True),
+        ("MIT", "5/TR-d.json", "static.counter5.TR.Json", False, True),
+        ("MIT", "5/TR-d.json", "static.counter5.TR.Json", True, False),
+        ("MIT", "5/TR-e.tsv", "static.counter5.TR.Tabular", True, True),
+        ("T&F ebooks", "5/TR_B1-a.xlsx", "static.counter5.TR.Tabular", True, True),
+        ("JSTOR", "5/IR_M1-a.csv", "static.counter5.IR_M1.Tabular", True, True),
+        ("JSTOR", "5/IR_M1-b.csv", "static.counter5.IR_M1.Tabular", False, True),
+        ("JSTOR", "5/IR_M1-b.csv", "static.counter5.IR_M1.Tabular", True, False),
+        ("JSTOR", "5/IR_M1-c.json", "static.counter5.IR_M1.Json", True, True),
+        ("JSTOR", "5/IR_M1-d.json", "static.counter5.IR_M1.Json", False, True),
+        ("JSTOR", "5/IR_M1-d.json", "static.counter5.IR_M1.Json", True, False),
     ),
 )
-def test_tsv(platform, file, parser, heuristics):
+def test_tsv(platform, file, parser, heuristics, success):
     source_path = pathlib.Path(__file__).parent / 'data/counter' / file
     output_path = pathlib.Path(__file__).parent / 'data/counter' / f"{file}.out"
     with output_path.open() as f:
         reader = csv.reader(f)
         poop = eat(source_path, platform, parsers=[parser], use_heuristics=heuristics)[0]
+
+        if not success:
+            assert isinstance(poop, NoParserMatchesHeuristics)
+            return
 
         for record in poop.records():
             assert next(reader) == list(record.as_csv()), "Compare lines"
