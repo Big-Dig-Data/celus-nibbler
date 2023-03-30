4b7fe894c84104216c253f665194c93a14f91fdf
Stepan Henek <stepan@bigdigdata.com>
2023-03-30 21:31:57 +0200

    chore: added official counter samples to tests

--- tests/test_counter5.py
+++ tests/test_counter5.py
@@ -17,6 +17,7 @@ from celus_nibbler.errors import NoParserMatchesHeuristics
         ("WebOfKnowledge", "5/DR-d.json", "static.counter5.DR.Json", False, True),
         ("WebOfKnowledge", "5/DR-d.json", "static.counter5.DR.Json", True, False),
         ("WebOfKnowledge", "5/DR-e.csv", "static.counter5.DR.Tabular", True, True),
+        ("WebOfKnowledge", "5/DR-sample.tsv", "static.counter5.DR.Tabular", True, True),
         ("MIT", "5/PR-a.tsv", "static.counter5.PR.Tabular", True, True),
         ("MIT", "5/PR-b.tsv", "static.counter5.PR.Tabular", False, True),
         ("MIT", "5/PR-b.tsv", "static.counter5.PR.Tabular", True, False),
@@ -24,6 +25,7 @@ from celus_nibbler.errors import NoParserMatchesHeuristics
         ("MIT", "5/PR-d.json", "static.counter5.PR.Json", False, True),
         ("MIT", "5/PR-d.json", "static.counter5.PR.Json", True, False),
         ("Ovid", "5/PR-e.csv", "static.counter5.PR.Tabular", True, True),
+        ("Ovid", "5/PR-sample.tsv", "static.counter5.PR.Tabular", True, True),
         ("MIT", "5/TR-a.tsv", "static.counter5.TR.Tabular", True, True),
         ("MIT", "5/TR-b.tsv", "static.counter5.TR.Tabular", False, True),
         ("MIT", "5/TR-b.tsv", "static.counter5.TR.Tabular", True, False),
@@ -31,6 +33,7 @@ from celus_nibbler.errors import NoParserMatchesHeuristics
         ("MIT", "5/TR-d.json", "static.counter5.TR.Json", False, True),
         ("MIT", "5/TR-d.json", "static.counter5.TR.Json", True, False),
         ("MIT", "5/TR-e.tsv", "static.counter5.TR.Tabular", True, True),
+        ("MIT", "5/TR-sample.tsv", "static.counter5.TR.Tabular", True, True),
         ("T&F ebooks", "5/TR_B1-a.xlsx", "static.counter5.TR.Tabular", True, True),
         ("JSTOR", "5/IR_M1-a.csv", "static.counter5.IR_M1.Tabular", True, True),
         ("JSTOR", "5/IR_M1-b.csv", "static.counter5.IR_M1.Tabular", False, True),
@@ -38,6 +41,7 @@ from celus_nibbler.errors import NoParserMatchesHeuristics
         ("JSTOR", "5/IR_M1-c.json", "static.counter5.IR_M1.Json", True, True),
         ("JSTOR", "5/IR_M1-d.json", "static.counter5.IR_M1.Json", False, True),
         ("JSTOR", "5/IR_M1-d.json", "static.counter5.IR_M1.Json", True, False),
+        ("JSTOR", "5/IR_M1-sample.tsv", "static.counter5.IR_M1.Tabular", True, True),
     ),
 )
 def test_tsv(platform, file, parser, heuristics, success):
