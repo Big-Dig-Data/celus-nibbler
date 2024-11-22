b055779930305b697c7bed2270cfdb0dbb035f47
Stepan Henek <stepan@bigdigdata.com>
2024-11-22 14:16:53 +0100

    chore: remove sample static parser (My...) they were never used and we prefer the dynamic parsers

--- celus_nibbler/parsers/non_counter/date_metric_based.py
+++ celus_nibbler/parsers/non_counter/date_metric_based.py
@@ -1,22 +1,10 @@
 import datetime
 import logging
-import re
 import typing
 from abc import ABCMeta
 
 from celus_nibbler.aggregator import SameAggregator
-from celus_nibbler.conditions import RegexCondition, SheetIdxCondition
-from celus_nibbler.coordinates import Coord, CoordRange, Direction
-from celus_nibbler.data_headers import DataFormatDefinition, DataHeaders
-from celus_nibbler.errors import TableException
 from celus_nibbler.parsers.base import BaseHeaderArea
-from celus_nibbler.sources import (
-    DateSource,
-    DimensionSource,
-    ExtractParams,
-    MetricSource,
-    OrganizationSource,
-)
 
 from .base import BaseNonCounterParser
 
@@ -37,47 +25,3 @@ class DateMetricBasedParser(BaseNonCounterParser):
     dimensions_to_skip: typing.Dict[str, typing.List[str]] = {}
 
     heuristics = None
-
-
-class MyDateMetricArea(BaseDateMetricArea):
-    dimensions_sources = {
-        "Extra": DimensionSource("Extra", CoordRange(Coord(1, 0), Direction.DOWN)),
-    }
-    organization_source = OrganizationSource(
-        CoordRange(Coord(1, 1), Direction.DOWN),
-        ExtractParams(
-            regex=re.compile(r"^MYCONS - (.*)$"),
-            on_validation_error=TableException.Action.STOP,
-        ),
-    )
-
-    data_headers = DataHeaders(
-        roles=[
-            DateSource(
-                CoordRange(Coord(0, 2), Direction.RIGHT),
-                ExtractParams(regex=re.compile(r"in (\d+\/\d+)$")),
-            ),
-            MetricSource(
-                CoordRange(Coord(0, 2), Direction.RIGHT),
-                ExtractParams(regex=re.compile(r"^([^ ]+.+[^ ]+) in")),
-            ),
-        ],
-        data_cells=CoordRange(Coord(1, 2), Direction.RIGHT),
-        data_direction=Direction.DOWN,
-    )
-
-    @property
-    def dimensions(self) -> typing.List[str]:
-        return list(self.dimensions_sources.keys())
-
-
-class MyDateMetricBasedParser(DateMetricBasedParser):
-    data_format = DataFormatDefinition(name="MY")
-    platforms = ["My Platform"]
-    heuristics = (
-        RegexCondition(re.compile(r"^Extra$"), Coord(0, 0))
-        & RegexCondition(re.compile(r"^Organization$"), Coord(0, 1))
-        & SheetIdxCondition(max=0)
-    )
-
-    areas = [MyDateMetricArea]
--- celus_nibbler/parsers/non_counter/metric_based.py
+++ celus_nibbler/parsers/non_counter/metric_based.py
@@ -1,21 +1,9 @@
 import datetime
-import re
 import typing
 from abc import ABCMeta
 
 from celus_nibbler.aggregator import SameAggregator
-from celus_nibbler.conditions import RegexCondition, SheetNameRegexCondition
-from celus_nibbler.coordinates import Coord, CoordRange, Direction
-from celus_nibbler.data_headers import DataFormatDefinition, DataHeaders
-from celus_nibbler.errors import TableException
 from celus_nibbler.parsers.base import BaseHeaderArea
-from celus_nibbler.sources import (
-    DateSource,
-    DimensionSource,
-    ExtractParams,
-    MetricSource,
-    OrganizationSource,
-)
 
 from .base import BaseNonCounterParser
 
@@ -36,44 +24,3 @@ class MetricBasedParser(BaseNonCounterParser):
     dimensions_to_skip: typing.Dict[str, typing.List[str]] = {}
 
     heuristics = None
-
-
-class MyMetricArea(BaseMetricArea):
-    date_source = DateSource(
-        CoordRange(Coord(15, 1), Direction.DOWN),
-        extract_params=ExtractParams(on_validation_error=TableException.Action.STOP),
-    )
-    dimensions_sources = {
-        "Dimension1": DimensionSource("Dimension1", CoordRange(Coord(15, 2), Direction.DOWN)),
-    }
-    organization_source = OrganizationSource(
-        CoordRange(Coord(15, 3), Direction.DOWN),
-        extract_params=ExtractParams(on_validation_error=TableException.Action.STOP),
-    )
-
-    data_headers = DataHeaders(
-        roles=[
-            MetricSource(
-                CoordRange(Coord(14, 7), Direction.RIGHT),
-            )
-        ],
-        data_cells=CoordRange(Coord(15, 7), Direction.RIGHT),
-        data_direction=Direction.DOWN,
-    )
-
-    @property
-    def dimensions(self) -> typing.List[str]:
-        return list(self.dimensions_sources.keys())
-
-
-class MyMetricBasedParser(MetricBasedParser):
-    data_format = DataFormatDefinition(name="MY")
-    platforms = ["My Platform"]
-    heuristics = (
-        RegexCondition(re.compile(r"^My Online Summary Usage Report$"), Coord(0, 0))
-        & RegexCondition(re.compile(r"^Dimension1$"), Coord(14, 2))
-        & RegexCondition(re.compile(r"^Organization$"), Coord(14, 3))
-        & ~SheetNameRegexCondition(re.compile(r"^ips_"))
-    )
-
-    areas = [MyMetricArea]
